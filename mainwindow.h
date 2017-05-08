0ea263ec2fc3d3f01e65d01db90e72300f704e8c
sanjeevan002 <sanjeevan002@gmail.com>
2017-05-08 12:33:34 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
 
 namespace Ui {
 class MainWindow;
@@ -13,17 +14,26 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    QString getImagePath();
+    QString saveImage();
+    void DisplayImage(QPixmap);
     ~MainWindow();
 
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_3_clicked();
+
     void on_pushButton_2_clicked();
+
+    void on_btnpixelize_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QImage *pimg;
-    QString saveFileName;
+    QGraphicsScene *scene;
+    QPixmap image;
+    QImage img;
 };
 
 #endif // MAINWINDOW_H
+
